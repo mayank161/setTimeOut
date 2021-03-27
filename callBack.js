c8ab8ebdcea1bdd47020324e3a74a154621fc965
Mayank <satimayank74@gmail.com>
2021-03-27 08:10:38 -0700

    count last edit time

--- callBack.js
+++ callBack.js
@@ -9,9 +9,11 @@ let users = [
 function getUser(){
     setTimeout(() => {
         let detail;
+        let count = 1;
         users.forEach((user) => {
             detail = `<h2>name = ${user.Name} email = ${user.Email}<h2>`;
             document.body.innerHTML += detail;
+            lastModify(count++);
         });
     }, 1000);
 }
@@ -23,9 +25,13 @@ function createUser(user,callBack) {
     },1000)
 }
 
-function create4thUser(user,createUser) {
-   
+function create4thUser(user,createUser,delay) {
+    setTimeout(() => {
         createUser(user,getUser);
-    
+    },delay);  
 }
-create4thUser({Name: 'Piush', Email: 'piush52@gmail.com'},createUser);
+
+function lastModify(count) {
+    document.getElementById('last').innerText = `last modification happened ${count} seconds ago`;
+}
+create4thUser({Name: 'Piush', Email: 'piush52@gmail.com'},createUser,1000);
